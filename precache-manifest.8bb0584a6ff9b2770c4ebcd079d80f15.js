b83ff77e0d5877f8b77b02dd8dcbd4daa3de9bde
Diego <dcosta.diego@gmail.com>
2019-12-25 21:12:36 -0300

    Updates

---
+++ precache-manifest.8bb0584a6ff9b2770c4ebcd079d80f15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb5a54bdf441e7de121949c9bc644ff",
+    "url": "/robotRolodex/index.html"
+  },
+  {
+    "revision": "5da77ae7e25fb8d4bacc",
+    "url": "/robotRolodex/static/css/main.49cce12f.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/robotRolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robotRolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5da77ae7e25fb8d4bacc",
+    "url": "/robotRolodex/static/js/main.8eceef2c.chunk.js"
+  },
+  {
+    "revision": "702815f6888d9ef64a20",
+    "url": "/robotRolodex/static/js/runtime-main.eace6449.js"
+  }
+]);
